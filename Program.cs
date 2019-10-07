e6da29f3d6c0d5e47197274545e92ce5f5f93fc3
MarcinSoltysiak <56115875+MarcinSoltysiak@users.noreply.github.com>
2019-10-07 22:01:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("wybierz jedną z opcji \n 1 \n 2");
+        int name = int.Parse(Console.ReadLine()); ;
+        switch (name)
+        {
+            case 1:
+                Console.WriteLine(1);
+                goto case 2;
+            case 2:
+                Console.WriteLine("stało się x \n wybierz: \n 1 \n 2"); //drugi wybór
+                
+        {
+                    int nam = int.Parse(Console.ReadLine()); ;
+                    switch (nam)
+                    {
+                        case 1:
+                            Console.WriteLine("stało się y");
+                            break;
+
+
+
+
+                    }
+
+
+
+
+                }
+            case 3:
+                Console.WriteLine(2);
+                goto case 4;
+            case 4:
+                Console.WriteLine("stało się x");
+                break;
+            
+
+
+        }
+    }
+}
