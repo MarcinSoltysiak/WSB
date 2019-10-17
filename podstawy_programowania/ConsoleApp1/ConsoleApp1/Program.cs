a723329e7c56993829f0cfe03593131ef0537edf
MarcinSoltysiak <56115875+MarcinSoltysiak@users.noreply.github.com>
2019-10-17 14:38:25 +0200

    Add files via upload

---
+++ podstawy_programowania/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tabliczka mnożenia");
+            int n = 10, row, col;
+            row = 1;
+            do
+            {
+                col = 1;
+                do
+                {
+                    Console.Write(row * col + "\t");
+                    col++;
+                } while (col <= n);
+                row++;
+                Console.WriteLine();
+            } while (row <= n);
+
+            
+        //napisz program który wyświetli duże litery od a do z i z do a z wykorzystaniem pętli for np A, B, C,... Z.
+
+            for (char A = 'A'; A <= 'Z';A++)
+            {
+                if (A != 'Z')
+                    Console.Write("");
+                else
+                {
+                    Console.Write(A + ".");
+                }
+                Console.Write(A);
+
+
+            }
+            for (char Z = 'Z'; Z >= 'A';Z-- )
+            {
+             if (Z != 'A')
+                {
+
+                }
+            }
+
+            //continue
+            int j = 1;
+            for (; j <= 10; j++)
+            {
+                if (j==2 || j == 5 || j == 7)
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine(j);
+                }
+            }
+            //break
+
+            int j = 10;
+            for (; j <= 10; j++)
+            {
+                if (j == 2 || j == 5 || j == 7)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(j);
+                }
+            }
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+        }
+    } //napisz program który za pomocą instrukcji do...while dla danych wartości zmieniających się w przedziale od 0 do 15 oblicz wartość funkcji y=4x
+    //napisz program zanjdujący się powyżej za pomocą pętli while
+
+}
---
+++ podstawy_programowania/tablice/tablice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace tablice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //napisz program który 10 elementowej tablicy 1 wymiarowej o nazwie tab umieszcza liczby całkowite z przedziału 1 do 10
+
+
+            int n = 10, i;
+
+            //deklaracja tablicy o nazwie tab typu całkowietego o rozmiarze n = 10
+            
+
+            //wyświetlenie zawartości tablicy
+            int[] tab = new int[n];
+
+            for (i = 0; i < tab.Length; i++)
+            {
+                Console.WriteLine("{0} ",tab[i]);
+            }
+            //napisz program który 5 elementowej tablicy 1 wymiarowej o nazwie colors umieszcza nazwy kolorów podane z klawiatury przez użytkownika, wyświetl kolory w formacie kolor1: red Kolor2: blue
+
+        }
+    }
+}
