51f1687e0204d580f50088470b2fdf0109cc0e5e
MarcinSoltysiak <56115875+MarcinSoltysiak@users.noreply.github.com>
2019-11-07 14:47:47 +0100

    Add files via upload

---
+++ podstawy_programowania/wyjątki 2/wyjątki 2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace wyjątki_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint number;
+            while(true)
+            {
+                Console.WriteLine("Wprowadz liczbę całkowitą: ");
+                string x = Console.ReadLine();
+                try
+                {
+                    number = uint.Parse(x);
+                    break;
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("Wprowadz liczbę w poprawnym formacie ");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("wprowadz liczbę w dopuszcalnym zakresie");
+                    Console.WriteLine("zakres liczb: <0;4 294967295>");
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("wartość nie może być pusta");
+
+                }
+                Console.WriteLine("wprowadź poprawnie wartość: ");
+
+            }
+            Console.WriteLine("Poprawna wartość: {0}",number);
+
+
+
+
+
+            //pobierz od użytkownika jakaś wartość od 1 do 5 
+
+            Console.ReadKey();
+        }
+    }
+}
