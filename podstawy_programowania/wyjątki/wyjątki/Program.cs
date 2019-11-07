93d7f042bad8d8cd30322f67972f3c61bee3c2e7
MarcinSoltysiak <56115875+MarcinSoltysiak@users.noreply.github.com>
2019-11-07 13:47:01 +0100

    Add files via upload

---
+++ podstawy_programowania/wyjątki/wyjątki/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wyjątki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //obsługa wyjątków
+            uint number;
+            while (true)
+                {
+                Console.WriteLine("wprowadz liczbę całkowitą: ");
+                string x = Console.ReadLine();
+                try
+                    {
+                        
+                        number = uint.Parse(x);
+                    break;
+
+                    }
+                    catch (SystemException e)
+                    {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e.Message);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("Podałeś błędne dane: ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(x);
+                    
+                    }
+
+                }
+           
+            Console.WriteLine("Prawidłowe dane: {0},", number);
+            Console.ForegroundColor = ConsoleColor.White;
+            
+            Console.ReadKey();
+
+        }
+    }
+}
