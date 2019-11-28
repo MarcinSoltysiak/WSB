d3e1034158aba4d5fd48f8edb65fd55db72caf34
MarcinSoltysiak <56115875+MarcinSoltysiak@users.noreply.github.com>
2019-11-28 14:46:25 +0100

    Add files via upload

---
+++ podstawy_programowania/wskazniki4/wskazniki4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wskazniki4
+{
+    class Program
+    {
+        struct test
+        {
+            public int n;
+        }
+
+
+            unsafe static void Main(string[] args)
+            {
+                test x = new test();
+                test* p = &x;
+            //p->n = 20;
+            p->n = 20;
+            (*p).n = 20;
+
+
+
+
+            Console.WriteLine(x.n);
+            Console.ReadKey();
+            }
+        
+    }
+}
