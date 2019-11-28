3feff36752fcb6bbca46efb0087bd6b3b54043f4
MarcinSoltysiak <56115875+MarcinSoltysiak@users.noreply.github.com>
2019-11-28 13:28:23 +0100

    Add files via upload

---
+++ podstawy_programowania/struktury/struktury/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace struktury
+{
+    class Program
+    {
+        public struct Punkt
+        {
+            public int x;
+            public int y;
+        }
+        static void Main(string[] args)
+        {
+            Punkt punkt1 = new Punkt();
+
+            punkt1.x = 5;
+            punkt1.y = 15;
+
+            Punkt punkt2;
+            punkt2.x = 2;
+            punkt2.y = 3;
+
+
+            Console.WriteLine("x jest równe: {0} a y jest równe {1}",punkt1.x,punkt1.y);
+
+            Console.WriteLine("x jest równe: {0} a y jest równe {1}", punkt2.x, punkt2.y);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ podstawy_programowania/wskaźniki/wskaźniki/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wskaźniki
+{
+    class Program
+    {
+        unsafe static void Main(string[] args)
+        {
+            char* x;
+            char letter = 'x';
+            x = &letter;
+            Console.WriteLine("znak: {0}", letter);
+            letter = 'a';
+
+            Console.WriteLine("znak: {0}",letter);
+            Console.WriteLine("znak: {0}",*x);
+            Console.WriteLine("adres pamięcie zmiennej letter: {0:x}", (int)x);
+
+            //Console.WriteLine("adres pamięcie zmiennej letter: {0:x}", (int)x); - system szesnastkowy
+
+            int test = 123;
+            Console.WriteLine(Convert.ToString(test,2));
+
+            //konwertowanie na dowolny system
+
+            double number;
+            double* numberAddress;
+
+            number = 10;
+            numberAddress = &number;
+
+            Console.WriteLine("Liczba wynosi: {0}", number);
+            Console.WriteLine("Liczba wynosi: {0}",numberAddress->ToString());
+
+            //->ToString() - inna wersja uzycia "*" przed zmienną
+
+            Console.WriteLine("Adres pamięci zmiennej numberAddress: {0}",(int)numberAddress);
+
+            Console.ReadKey();
+
+        }
+    }
+}
