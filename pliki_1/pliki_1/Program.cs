c24e1dfdae90082b78659b3673f3b51c1bc6db69
MarcinSoltysiak <56115875+MarcinSoltysiak@users.noreply.github.com>
2019-12-05 14:47:09 +0100

    Add files via upload

---
+++ pliki_1/pliki_1/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+
+namespace pliki_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo wsbDir = new DirectoryInfo(".");
+            Console.WriteLine(wsbDir.FullName);
+
+            //DirectoryInfo studentDir = new DirectoryInfo("C:\Users\Student\Desktop");
+
+            DirectoryInfo studentDir = new DirectoryInfo(@"C:\Users\Student");
+            //ścieżka
+            Console.WriteLine(studentDir.FullName);
+            //nazwa folderu
+            Console.WriteLine(studentDir.Name);
+            //folder nadrzędny 
+            Console.WriteLine(studentDir.Parent);
+            //właściwości
+            Console.WriteLine(studentDir.Attributes);
+            //kiedy stworzony
+            Console.WriteLine(studentDir.CreationTime);
+
+            DirectoryInfo dataDir = new DirectoryInfo(@"C:\Users\Student\C#Files");
+
+            string[] clients =
+            {
+                "Agnieszka Nowak",
+                "Anna Nowak",
+                "Tomasz Kowalski",
+            };
+            //tworzenie pliku 1
+            
+
+            string path = @"C:\Users\student\C#Files";
+            //instrukcja warukowa tworzenia pliku jeżeli nie istnieje
+            try
+            {
+                if (Directory.Exists(path))
+                {
+                    Console.WriteLine("Katalog istnieje");
+                }
+                else
+                {
+                    string textFilePath = @"C:\Users\student\C#Files\testFile1.txt";
+                    Directory.CreateDirectory("\\users\\student\\C#Files");
+                    //dodawanie do pliku
+                    File.WriteAllLines(textFilePath, clients);
+                    Console.WriteLine("\nutworzono katalog: {0}", Directory.GetCreationTime(path));
+
+
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("error: {0}", e.ToString());
+            }
+            //Czy chcesz usunąć katalog 1-true  0-false, 1 - sprawdzamy czy usunięty 0 - nie usunałeś katalogu
+            Console.WriteLine("CZy chcesz usunąć plik: \n1 - tak\n0 - nie");
+            int x = int.Parse(Console.ReadLine());
+            try
+            {
+                
+                switch (x)
+                {
+                    case 1:
+                        if (Directory.Exists(path))
+                        {
+                            Directory.Delete(path, true);
+                            Console.WriteLine("Usunąłeś plik");
+                            if (!Directory.Exists(path))
+                            {
+                                Console.WriteLine("Plik został usunięty");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Plik nie został usunięty");
+                            }
+                        }
+                        else
+                        {
+
+                        }
+                        break;
+                    case 0:
+                        Console.WriteLine("Nie usunąłeś pliku");
+                        break;
+                }
+            }
+            catch (SystemException)
+            {
+                Console.WriteLine("Podałeś złe dane");
+            }
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ pliki_3/pliki_3/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+
+namespace pliki_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo wsbDir = new DirectoryInfo(".");
+            Console.WriteLine(wsbDir.FullName);
+
+            //DirectoryInfo studentDir = new DirectoryInfo("C:\Users\Student\Desktop");
+
+            DirectoryInfo studentDir = new DirectoryInfo(@"C:\Users\Student");
+            //ścieżka
+            Console.WriteLine(studentDir.FullName);
+            //nazwa folderu
+            Console.WriteLine(studentDir.Name);
+            //folder nadrzędny 
+            Console.WriteLine(studentDir.Parent);
+            //właściwości
+            Console.WriteLine(studentDir.Attributes);
+            //kiedy stworzony
+            Console.WriteLine(studentDir.CreationTime);
+
+            DirectoryInfo dataDir = new DirectoryInfo(@"C:\Users\Student\C#Files");
+
+            string[] clients =
+            {
+                "Agnieszka Nowak",
+                "Anna Nowak",
+                "Tomasz Kowalski",
+            };
+            //tworzenie pliku 1
+
+
+            string path = @"C:\Users\student\C#Files";
+            //instrukcja warukowa tworzenia pliku jeżeli nie istnieje
+            try
+            {
+                if (Directory.Exists(path))
+                {
+                    Console.WriteLine("Katalog istnieje");
+                }
+                else
+                {
+                    string textFilePath = @"C:\Users\student\C#Files\testFile1.txt";
+                    Directory.CreateDirectory("\\users\\student\\C#Files");
+                    //dodawanie do pliku
+                    File.WriteAllLines(textFilePath, clients);
+                    Console.WriteLine("\nutworzono katalog: {0}", Directory.GetCreationTime(path));
+
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("error: {0}", e.ToString());
+            }
+            //Czy chcesz usunąć katalog 1-true  0-false, 1 - sprawdzamy czy usunięty 0 - nie usunałeś katalogu
+
+
+            DirectoryInfo myDataDir = new DirectoryInfo(path);
+
+            Console.WriteLine();
+
+            FileInfo[] txtFiles = myDataDir.GetFiles("*.txt", SearchOption.AllDirectories);
+
+            Console.WriteLine("Ilość plików txt: {0}",txtFiles.Length);
+
+            Console.WriteLine("Pliki tekstowe");
+
+            foreach (FileInfo file in txtFiles)
+            {
+                Console.WriteLine(file.Name);
+                Console.WriteLine(file.Length);
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
